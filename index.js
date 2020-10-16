8c68b02c0c5603aedb58dd21cabdbd6546516baf
SydBal <dominic.balassone@gmail.com>
2020-10-15 22:28:19 -0700

    added color reverse into image click along with some button-like styles

---
+++ index.js
@@ -0,0 +1,5 @@
+const html = document.documentElement
+
+/* Avatar Button */
+const avatarButton = document.getElementById('avatar-button')
+avatarButton.addEventListener('click', () => html.classList.toggle('inverse'), false)
