ce2c603ef9935b22a126cf32992b3f08ca2da0f5
SydBal <dominic.balassone@gmail.com>
2020-10-15 23:35:32 -0700

    reverse world initialized

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const html = document.documentElement
 
 /* Avatar Button */
 const avatarButton = document.getElementById('avatar-button')
-avatarButton.addEventListener('click', () => html.classList.toggle('inverse'), false)
+avatarButton.addEventListener('click', () => {
+  html.classList.toggle('inverse')
+}, false)
